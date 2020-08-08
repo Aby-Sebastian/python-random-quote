447529ac6afa7e788a6a28971b67bba15acfcf23
Aby-Sebastian <abysebastian98@gmail.com>
2020-08-08 23:14:01 +0530

    completed

--- get-quote.py
+++ get-quote.py
@@ -4,11 +4,12 @@ def run():
     
     f = open("quotes.txt")
     quotes = f.readlines()
-    last = 13
-    rnd = random.randint(0,last)
+    last = len(quotes)
+    rnd = random.sample(range(0,last),2)  #generates multiple quotes randomly
     f.close()
 
-    print(quotes[rnd])
+    print(quotes[rnd[0]],end='')
+    print(quotes[rnd[1]])
 
 if __name__== "__main__":
   run()
